a0824db1342fdd0346669d3ee3f483c3bdc6c6bd
ConleyGao <haogao@umass.edu>
2018-04-11 15:46:28 -0400

    Update Main_process.c

--- Main_process.c
+++ Main_process.c
@@ -27,132 +27,139 @@
     volatile short * Video_Mem_ptr  = (short *) FPGA_ONCHIP_BASE;
     volatile short * TXT    = (short *) TEXT;
     volatile int * SW_ptr               = (int *) SW_BASE;
-	
-	
-	char one_bit_pict[ROW*COL] ; 	//every pixel is stored as a char, should be 1 or 0, in a quashed 1D array
-
+    
+    int finish = 0;    // global flag for finish
+    char one_bit_pict[ROW*COL] ;    //every pixel is stored as a char, should be 1 or 0, in a quashed 1D array
+    //compressed data
+int combuff[ROW*COL];
+    // Number of compressed 
+    N =0;
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 
-int invert(){
-	/*
-	* Capture image and convert into black and white 0x0000 or 0xFFFF
-	* in place operation 
-	*/
+int preProcess(){
+    /*
+    * Capture image and convert into black and white 0x0000 or 0xFFFF
+    * in place operation 
+    */
+    /*
+    * TODO: convert all black and white pixel into single digit representation, 1 and 0
+    * Store in a some data structure(maybe glocbal)
+    * NOTE: This is required by the instruction. We can do this in while compressing, but we can't 
+    */
+    
     int x,y;
-	short arr[ROW][COL];
-	int count = 0
-	*(Video_In_DMA_ptr + 3) = 0x0;          // Disable the video to capture one frame
+    short arr[ROW][COL];
+    int count = 0
+    *(Video_In_DMA_ptr + 3) = 0x0;          // Disable the video to capture one frame
     for (y = 0; y < 240; y++) {
         for (x = 0; x < 320; x++) {
             short temp2 = *(Video_Mem_ptr + (y << 9) + x);
-			arr[y][x] = temp2;
+            arr[y][x] = temp2;
             if(temp2 > 8210){
                 temp2 = 0;
-				one_bit_pict[count] = 0;
-				count++;
+                one_bit_pict[count] = 0;
+                count++;
             }
             else if(temp2 < 8210){
                 temp2 = 0xFFFF;
-				one_bit_pict[count] = 1;
-				count++;
+                one_bit_pict[count] = 1;
+                count++;
             }
             *(Video_Mem_ptr + (y << 9) + x) = temp2;
-        }	
+        }   
     }
-	
-	return 0;
-}
-
-
-
-///////////////////////////////////////////////////////////////////////////////
-//////////////////////Gongtao's functions//////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////
-void Welcome(){
-	char* welcome = "Press to take picture, and press again to process";
-    int offset;
     
-    offset = ( 1 << 7) + 1; 
-    while ( *(welcome) )
-    {
-        *(TXT + offset) = *(welcome); // write to the character buffer
-        //printf("%s",*(TXT + offset));
-        ++welcome;
-        ++offset;
-    }
-  
+    return 0;
 }
 
- 
-/*
-* main code below, image() and  main()
-*/
-int convert(){
-	/*
-	* TODO: convert all black and white pixel into single digit representation, 1 and 0
-	* Store in a some data structure(maybe glocbal)
-	* NOTE: This is required by the instruction. We can do this in while compressing, but we can't 
-	*/
-	
-	return 0;
-}
+
+
 
 int compress(){
-	/*
-	*TODO: package the picture into stream of 8-bit segment and send them one to the compressor
-	*all compressing handling are done in here 
-	*/
-	char x = one_bit_pict[0];
-	for(int i = 1; i < 8; i++){
-		x = x + one_bit_pict[i+counter_for_byte*8];
-		x = (x << 1);
-	}
-	if(FIFO_IN_FULL_PIO == 0){
-			alt_write_byte(ALT_FPGA_BRIDGE_LWH2F_OFST + FIFO_IN_WRITE_REQ_PIO_BASE, 1);
-			alt_write_byte(ODATA_PIO, x);
-	}
-	while(FIFO_IN_FULL_PIO == 1){
-		if(FIFO_IN_FULL_PIO == 0){
-			alt_write_byte(ALT_FPGA_BRIDGE_LWH2F_OFST + FIFO_IN_WRITE_REQ_PIO_BASE, 1);
-			alt_write_byte(ODATA_PIO, x);
-	}
-	counter_for_byte ++;
-	return 0;
+    /*
+    *TODO: package the picture into stream of 8-bit segment and send them one to the compressor
+    *all compressing handling are done in here 
+    */
+    
+    return 0;
 }
 
+int decompress(){
 
+//take 24 bit number to binary
+    Rready = alt_read_byte(ALT_FPGA_BRIDGE_LWH2F_OFST+RESULT_READY_PIO_BASE);
+    if (Rready){
+    alt_write_byte(ALT_FPGA_BRIDGE_LWH2F_OFST + FIFO_OUT_READ_REQ_PIO_BASE, 1);
+    output =alt_read_hword(ALT_FPGA_BRIDGE_LWH2F_OFST+IDATA_PIO_SPAN);
+    decode(out
+    combuff[N] = output;
+    }
+    return 0;
+}
+int decode(){
+    int i =0;
+    int j =0;
+    int data =0;
+    int type =0;
+    int current =0;
+     for(i=0; i<N+1; i++){
+      data = combuff[0];
+       type = data>>23;
+       count  = data- ((data>>23)<<23)
+       
+       for (j= 0; j<count; j++){
+           if(type = 0){
+          *(Video_Mem_ptr +current  = 0x0000
+           }
+           if(type =1){
+           *(Video_Mem_ptr + current =0xFFFF
+           }
+       current ++;
+     
+     }
+    
+}
+int combuff[ROW*COL];
+    return 0;
+}
 int process(void){
-	/*
-	* This is the moddified old image(), same idea, press once to capture, and no switch needed
-	* press again to quit 
-	*/
-	
-	*(Video_In_DMA_ptr + 3) = 0x4;              // Enable the video every time 
-	while(*KEY_ptr != 0 );						// catch long KEY holding, wait for release
+    /*
+    * This is the moddified old image(), same idea, press once to capture, and no switch needed
+    * press again to quit 
+    */
+    
+    *(Video_In_DMA_ptr + 3) = 0x4;              // Enable the video every time 
+    while(*KEY_ptr != 0 );                      // catch long KEY holding, wait for release
 
-		
+        
     while (1)
     {
         if (*KEY_ptr != 0)                      // check if any KEY was pressed
         {   
             
-				invert();
-				/*
-				* processing code here, 
-				*/
-				convert();
-				compress();
-				
-				while(*KEY_ptr != 0 );
+                invert();
+                preProcess();
+                /*
+                * processing code here, 
+                */
+                int n;
+                while(!finish){ // doing stuff
+                
+                    compress();
+                   
+                    
+                }
+                while(*KEY_ptr != 0 );
+                decode
                 while(*KEY_ptr == 0 );          //wait for KEY press to end this cycle 
                 return 0;
             
-		}
-	}
-	
-	return 0;
+        }
+    }
+    
+    return 0;
 }
 
 
@@ -161,14 +168,15 @@ int main(void){                                             //Da main code
     /*
     * HERE comes da main code 
     */
-	int y;
-	text_ptr = " ";
+    int y;
+    text_ptr = " ";
     while(1){
-        process();      
-		for(y = 0 ; y<(80*60); y++){
-			*(TXT + y) = *(text_ptr); // write to the character buffer
-		}
-	}		
+        process();  
+            
+        for(y = 0 ; y<(80*60); y++){
+            *(TXT + y) = *(text_ptr); // write to the character buffer
+        }
+    }       
     
     return 0;                                               //shouldn't reach here 
 }
